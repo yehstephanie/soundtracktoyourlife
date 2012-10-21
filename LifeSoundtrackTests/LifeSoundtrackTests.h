bf232098bbd6f8bf26a7ceff079f53dc4b5283f1
Stephanie Yeh <yehstephanie@gmail.com>
2012-10-21 11:20:59 -0700

    first commit

---
+++ LifeSoundtrackTests/LifeSoundtrackTests.h
@@ -0,0 +1,13 @@
+//
+//  LifeSoundtrackTests.h
+//  LifeSoundtrackTests
+//
+//  Created by Stephanie Yeh on 10/17/12.
+//  Copyright (c) 2012 Stephanie Yeh. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LifeSoundtrackTests : SenTestCase
+
+@end
---
+++ LifeSoundtrackTests/LifeSoundtrackTests.m
@@ -0,0 +1,32 @@
+//
+//  LifeSoundtrackTests.m
+//  LifeSoundtrackTests
+//
+//  Created by Stephanie Yeh on 10/17/12.
+//  Copyright (c) 2012 Stephanie Yeh. All rights reserved.
+//
+
+#import "LifeSoundtrackTests.h"
+
+@implementation LifeSoundtrackTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LifeSoundtrackTests");
+}
+
+@end
